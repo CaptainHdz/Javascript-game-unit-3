dd8f15ba76304ff2d3d09a9a01aa37c2be256b44
unknown <mauro.hdz99@gmail.com>
2019-03-13 13:21:27 -0500

    Attempted it

---
+++ Assets/javascript/game.js
@@ -0,0 +1,15 @@
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var words = ["Zeus", "Hades", "Poseidon", "Giants", "Medusa", "Heracles", "Achilles", "Cerberus", "Minotaur", "Centaur", "Kratos", "Pegasus", "Hydra", "Gorgon", "Hermes", "Phoenix"];
+
+var pcword = words[math.floor(math.random() * words.length ) ]
+console.log
+var wordchoice = pcword.length
+
+
+
+var usranswer= document.write("Pick a letter");
+
+
+
+
+if (letters.indexOf(usranswer)); 
